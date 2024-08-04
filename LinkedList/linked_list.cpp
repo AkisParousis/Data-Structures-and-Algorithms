4ae9e29fdf4e75315b63a3882e262f6143ca4efc
AkisParousis <33992774+AkisParousis@users.noreply.github.com>
2024-08-04 16:43:28 +0300

    Create linked_list.cpp

---
+++ LinkedList/linked_list.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node* next;
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class LinkedList {
+    private:
+        Node* head;
+        int length;
+        
+    public:
+        LinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            length = 1;
+        }
+
+        ~LinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty";
+            } else {
+                while (temp != nullptr) {
+                    cout << temp->value;
+                    temp = temp->next;
+                    if (temp != nullptr) {
+                        cout << " -> ";
+                    }
+                }
+            }
+            cout << endl;
+        }
+
+        Node* getHead() {
+            return head;
+        }
+
+        int getLength() {
+            return length;
+        }
+        
+        void makeEmpty() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+            length = 0;
+        }
+
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (head == nullptr) {
+                head = newNode;
+            } else {
+                Node* currentNode = head;
+                while (currentNode->next != nullptr) {
+                    currentNode = currentNode->next;
+                }
+                currentNode->next = newNode;
+            }
+            length++;
+        }
+
+        void partitionList(int x) {
+            LinkedList* big = new LinkedList(0);
+            big->makeEmpty();
+            LinkedList* small = new LinkedList(0);
+            small->makeEmpty();
+            Node* temp = this->head;
+            while (temp!= nullptr) {
+                if (temp->value >= x) {
+                    big->append(temp->value);
+                } else {
+                    small->append(temp->value);
+                }
+                temp = temp->next;
+            }
+            temp = big->head;
+            while (temp!= nullptr) {
+                small->append(temp->value);
+                temp = temp->next;
+            }
+
+            this->head = small->head;
+        }
+
+};
