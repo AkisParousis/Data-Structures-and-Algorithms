61e66228109f5f5d6342f9525fedcf5c5a5819e4
AkisParousis <33992774+AkisParousis@users.noreply.github.com>
2024-08-10 20:12:33 +0300

    Update double_linked_list.cpp

--- DoubleLinkedList/double_linked_list.cpp
+++ DoubleLinkedList/double_linked_list.cpp
@@ -31,7 +31,19 @@ class DoubleLinkedList {
             tail = newNode;
             length = 1;
         }
-    
+
+    Node* getHead() {
+        return head;
+    }
+
+    Node* getTail() {
+        return tail;
+    }
+
+    int getLength() {
+        return length;
+    }
+
     void append(int value) {
         Node* newNode = new Node(value);
         if (head == nullptr) {
@@ -43,6 +55,18 @@ class DoubleLinkedList {
             tail = newNode;
         }
         length++;
+    }
+
+    void makeEmpty() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* nodeToDelete = current;
+            current = current->next;
+            delete nodeToDelete;
+        }
+        head = nullptr;
+        tail = nullptr;
+        length = 0;
     }
 
     void deleteLast() {
@@ -77,14 +101,99 @@ class DoubleLinkedList {
         cout << endl;
     }
 
+    void prepend(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode; 
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+            if (length == 1){
+                tail=head->next;
+            }
+        }
+        length++;
+    }
+
+    void deleteFirst() {
+        if (head == nullptr) {
+            return;
+        }
+        if (head->next == nullptr) {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+        } else {
+            head = head->next;
+            delete head->prev;
+            head->prev = nullptr;
+        }
+    }
+
+    Node* get(int index) {
+        if (index < 0 || index >= length || length==0) {
+            return nullptr;
+        }
+        
+        Node* temp = head;
+        while (index > 0) {
+            temp = temp->next;
+            index--;
+        }
+        
+        return temp;
+    }
+
+    bool insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+            Node* newNode = new Node(value);
+        if (index == 0) {
+            this->prepend(value);
+            return true;
+        }
+        if (index == length) {
+            this->append(value);
+            return true;
+        } else {
+            Node* temp = this->get(index);
+            newNode->prev = temp->prev;
+            temp->prev = newNode;
+            newNode->next = temp;
+            newNode->prev->next = newNode;
+            length++;
+            return true;
+        }
+    }
+
+    void deleteNode(int index) {
+        if (index < 0 || index > length) return;
+        if (index == 0) {
+            deleteFirst();
+            return;
+        } 
+        if (index == length-1) {
+            deleteLast();
+        } else {
+            Node* temp = get(index);
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+            delete temp;
+            length--;
+        }
+        return;
+    }
+
+
 
 };
 
 int main() {
-    DoubleLinkedList dll(10);
-    dll.append(20);
-    dll.append(30);
-    dll.append(40);
-    dll.deleteLast();
-    dll.printList();
+    DoubleLinkedList dll(5);
+    dll.append(10);
+    dll.append(15);
+
+    dll.insert(1, 100);
+    cout << endl;
 }
